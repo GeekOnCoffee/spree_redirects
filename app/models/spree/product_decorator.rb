8863926c470cf755dc2a3a27fc4e5a55df6a2e0e
Andrew Hooker <GeekOnCoffee@gmail.com>
2012-09-13 08:13:46 -0500

    fixing products

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -1,8 +1,9 @@
 Spree::Product.class_eval do
   before_update :create_redirect
   def create_redirect
+
     return true unless name_changed?
-    save_permalink(self.name)
+    save_permalink(self.name.to_s.to_url)
     return true unless permalink_changed?
     redirect = Spree::Redirect.find_or_initialize_by_old_url("/products/#{self.permalink_was}")
     redirect.new_url = "/products/#{self.permalink}"
