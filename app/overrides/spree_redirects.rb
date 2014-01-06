98361667d775e938819259bb5959320e2e9a27fe
Andrew Hooker <geekoncoffee@gmail.com>
2014-01-06 13:42:42 -0600

    Removing defunkt url

--- app/overrides/spree_redirects.rb
+++
@@ -1,5 +0,0 @@
-Deface::Override.new(:virtual_path  => "spree/admin/shared/_configuration_menu",
-                     :name          => "spree_redirect_config_opt",
-                     :insert_bottom => "[data-hook='admin_configurations_sidebar_menu']",
-                     :text          => "<%= configurations_sidebar_menu_item t(:redirects), admin_redirects_path %>",
-                     :disabled      => false)
